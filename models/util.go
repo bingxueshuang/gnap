84a9f950b41e2c76dd283588da8a68a115f61d22
Manojna <manojna.gopher@gmail.com>
2023-04-25 15:22:27 +0530

    restore(models): remove time utils
    - it is silly to add a bulky wrapper around something
      unnecessary.

--- models/util.go
+++ models/util.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
-	"time"
 )
 
 // ErrInvalidURL is the error returned in case of invalid URL
@@ -40,7 +39,7 @@ func (u URL) MarshalJSON() ([]byte, error) {
 	if u.URL == nil || u.String() == "" {
 		return nil, ErrInvalidURL
 	}
-	return []byte(u.String()), nil
+	return json.Marshal(u.String())
 }
 
 // ParseURL is a helper function for creating [URL].
@@ -52,36 +51,4 @@ func ParseURL(raw string) (URL, error) {
 		return URL{}, fmt.Errorf("%w: %w", ErrInvalidURL, err)
 	}
 	return URL{u}, nil
-}
-
-// Seconds a wrapper around [time.Duration] to store
-// the number of seconds only.
-type Seconds time.Duration
-
-// UnmarshalJSON implements the [json.Unmarshaler] interface.
-func (s *Seconds) UnmarshalJSON(data []byte) error {
-	var seconds int64
-	err := json.Unmarshal(data, &seconds)
-	if err != nil {
-		return err
-	}
-	*s = Seconds(seconds * int64(time.Second))
-	return nil
-}
-
-// MarshalJSON implements the [json.Marshaler] interface.
-func (s Seconds) MarshalJSON() ([]byte, error) {
-	seconds := int(time.Duration(s).Seconds())
-	return json.Marshal(seconds)
-}
-
-// Seconds is a convenience wrapper around [time.Duration.Seconds].
-func (s Seconds) Seconds() int {
-	return int(time.Duration(s).Seconds())
-}
-
-// NewSeconds is a convenience constructor for creating
-// [Seconds] from numerical seconds.
-func NewSeconds(seconds int) Seconds {
-	return Seconds(int64(seconds) * int64(time.Second))
 }
